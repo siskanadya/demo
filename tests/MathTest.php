3d033011f5dab5e7ca45ea5765b2cc16870860f2
Fransiska <nadya.fransiska@gmail.com>
2025-10-27 11:52:03 +0100

    repare phpunit

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -3,7 +3,7 @@
 use PHPUnit\Framework\TestCase;
 use App\Math;
 
-class MathTest extends TestCase
+final class MathTest extends TestCase
 {
     private Math $math;
 
@@ -12,27 +12,27 @@ class MathTest extends TestCase
         $this->math = new Math();
     }
 
-    public function testAddition()
+    public function testAddition(): void
     {
         $this->assertEquals(8, $this->math->addition(5, 3));
     }
 
-    public function testSoustraction()
+    public function testSoustraction(): void
     {
         $this->assertEquals(2, $this->math->soustraction(5, 3));
     }
 
-    public function testMultiplication()
+    public function testMultiplication(): void
     {
         $this->assertEquals(15, $this->math->multiplication(5, 3));
     }
 
-    public function testDivision()
+    public function testDivision(): void
     {
         $this->assertEquals(5, $this->math->division(10, 2));
     }
 
-    public function testDivisionByZeroThrowsException()
+    public function testDivisionByZeroThrowsException(): void
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->math->division(10, 0);
